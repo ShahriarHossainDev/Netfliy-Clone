f3c437a0011913dfd88334d0398360891ed53fc3
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2023-02-21 21:28:57 +0600

    TitleTableViewCell

---
+++ Netfliy Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netfliy Clone
+//
+//  Created by Shishir_Mac on 21/2/23.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
